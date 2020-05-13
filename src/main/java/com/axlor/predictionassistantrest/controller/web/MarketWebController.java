b4c32c8ede35ea49a3d79058fc599281d0f58445
axlor <axlor852@gmail.com>
2020-05-13 16:00:32 -0500

    Finished market info and contract history pages and their controllers. Small modifications to existing pages.
    Todo: Make a useful index page that allows searching for Markets/Contracts containing search term(s), other useful features.
    
    Todo: Style pages.
    
    Todo: Ideally someday the REST api would be managed by an app and the web frontend would be dynamic, probably from Angular, and the building/managing of the DOM would be handled by the user's browser, not built by Thymeleaf on the server side, but this is just for now.

---
+++ src/main/java/com/axlor/predictionassistantrest/controller/web/MarketWebController.java
@@ -0,0 +1,76 @@
+package com.axlor.predictionassistantrest.controller.web;
+
+import com.axlor.predictionassistantrest.exception.MarketNotFoundException;
+import com.axlor.predictionassistantrest.exception.NoSnapshotsInDatabaseException;
+import com.axlor.predictionassistantrest.model.Contract;
+import com.axlor.predictionassistantrest.model.Market;
+import com.axlor.predictionassistantrest.service.MarketService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class MarketWebController {
+
+    @Autowired
+    MarketService marketService;
+
+    @RequestMapping("/markets/{id}")
+    public String getSnapshotInfo(@PathVariable("id") String id, Model model){
+        try{
+            int nonUniqueId = Integer.parseInt(id);
+            Market market = marketService.getLatestMarketInfo(nonUniqueId);
+            //could just add the market object but I think this makes it cleaner in the html
+            model.addAttribute("nonUniqueId", nonUniqueId);
+            model.addAttribute("marketName", market.getName());
+            model.addAttribute("contractsList", market.getContracts());
+            model.addAttribute("marketUrl", market.getUrl());
+
+            return "marketInfo";
+        }catch(NumberFormatException nfe){
+            return "invalidMarketIdError";
+        } catch (NoSnapshotsInDatabaseException | MarketNotFoundException e) {
+           return "marketNotFound404Page";
+        }
+    }
+
+    @RequestMapping("/markets/{marketId}/contracts/{contractId}")
+    public String getContractHistory(@PathVariable("marketId") String marketId, @PathVariable("contractId") String contractId, Model model){
+        try{
+            int nonUniqueMarketId = Integer.parseInt(marketId);
+            int nonUniqueContractId = Integer.parseInt(contractId);
+
+            List<Market> marketHistory = marketService.getMarketHistoryByNonUniqueId(nonUniqueMarketId);
+            List<Contract> contractHistoryList = new ArrayList<Contract>();
+            for (int i = 0; i < marketHistory.size(); i++) {
+                for (int j = 0; j < marketHistory.get(i).getContracts().size(); j++) {
+                    if(marketHistory.get(i).getContracts().get(j).getId() == nonUniqueContractId){
+                        contractHistoryList.add(marketHistory.get(i).getContracts().get(j));
+                    }
+                }
+            }
+            if(contractHistoryList==null || contractHistoryList.isEmpty()){
+                return "marketNotFound404Page";
+            }
+
+            model.addAttribute("contractHistoryList", contractHistoryList);
+            model.addAttribute("contractId", nonUniqueContractId);
+            model.addAttribute("marketName", marketHistory.get(0).getName());
+            model.addAttribute("contractName", contractHistoryList.get(0).getName());
+            model.addAttribute("marketHistoryList", marketHistory);
+
+            return "contractHistory";
+
+        }catch(NumberFormatException nfe){
+            return "invalidMarketIdError";
+        } catch (MarketNotFoundException e) {
+            return "marketNotFound404Page";
+        }
+    }
+
+}
